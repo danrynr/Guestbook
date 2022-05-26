8ea858a47d3f8356154f03cb609185ca80e313e7
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-05-26 18:57:26 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@
     <?php
         session_start();
         
-        if (empty($_SESSION['sadmin_username'])) {
+        if (empty($_SESSION['admin_username'])) {
             header('location: login.php');
         }
 
@@ -20,17 +20,18 @@
         $pageNum = 1;
 
         if (isset($_GET['page'])) {
-            $pageNum = $_GET['page'];
+            $pageNum = intval($_GET['page']);
         }
 
         $offset = ($pageNum - 1) * $rowsPerPage;
-        $query = "SELECT * FROM visitors ORDER BY id DESC LIMIT $offset, $rowsPerPage";
-        $result = mysqli_query($conn, $query) or die ('Error, query failed');
 
-        $query1 = "SELECT COUNT(id) AS numrows FROM visitors";
-        $result1 = mysqli_query($conn, $query1) or die ('Error, query failed');
-        $row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC);
-        $numrows = $row1['numrows'];
+        $query = "SELECT * FROM visitors ORDER BY id DESC LIMIT $offset, $rowsPerPage";
+        $result = mysqli_query($conn, $query) or die ('Error, query failed 1');
+
+        $query1 = "SELECT id FROM visitors";
+        $result1 = mysqli_query($conn, $query1) or die ('Error, query failed 2');
+
+        $numrows = mysqli_num_rows($result1);
         echo "Total Number Guestbook : $numrows";
     ?>
 
@@ -38,8 +39,9 @@
     <a href="logout.php">Logout</a>
     
     <?php
-        $no = 1;
-        while ($hasil = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $no = 0;
+        while ($no < mysqli_num_rows($result)) {
+            $hasil = mysqli_fetch_array($result);
             echo '<table width="90%" border="1" align="center" cellpadding="2" cellspacing="2" class="content">';
             echo '<tr valign="top" bgcolor="FFDFAA">';
             echo '<td width="8%"><div align="center"><strong>No</strong></div></td>';
@@ -49,6 +51,7 @@
             echo "</tr>";
 
             echo "<tr valign='top'>";
+            echo  "<td>$no</td>";
             echo "<td>From: ".$hasil['name']."<br>".$hasil['comment']."</td>";
             echo "<td><a href='delete.php?del=".$hasil['id']."'>Delete</a></td>";
             echo "<td><a href='update.php?updt=".$hasil['id']."'>Update</a></td>";
@@ -57,42 +60,43 @@
             $no++;
         }
 
-        $query = "SELECT COUNT(id) AS NUMROWS FROM visitors";
-        $result = mysqli_query($conn, $query) or die ('Error, query failed');
+        $query = "SELECT id FROM visitors";
+        $result = mysqli_query($conn, $query) or die ('Error, query failed 3');
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $numrows = $row['numrows'];
-
+        $numrows = mysqli_num_rows($result);
         $maxPage = ceil($numrows/$rowsPerPage);
-        $self = $_SERVER['PHP_SELF'];
-        $nav = '';
 
-        for ($page = 1; $page <= $maxPage; $page++) {
+        $nav = "";
+        $page = 1;
+        $self = $_SERVER['PHP_SELF'];
+    
+        for ($i = 1; $i < 0; $i++) {
             if ($page == $pageNum) {
-                $nav .= "$page";
+                $nav .= " $page ";
             } else {
-                $nav .= " <a href=\"$self?page=$page\">$page</a> ";
+                $nav .= " <a href='$self?page=$page'>$page</a> ";
             }
         }
-
-        if ($pageNum > 1) {
+    
+        if ($pageNum > 1 ) {
             $page = $pageNum - 1;
-            $prev = " <a href=\"$self?page=$page\">[Prev]</a> ";
-            $first = " <a href=\"$self?page=1\">[First Page]</a> "; 
+            $prev = " <a href='$self?page=$page'>[Prev]</a> ";
+            $first = " <a href='$self?page=1'>[First Page]</a> ";
         } else {
             $prev = '&nbsp;';
             $first = '&nbsp;';
         }
-
+    
         if ($pageNum < $maxPage) {
             $page = $pageNum + 1;
-            $next = " <a href=\"$self?page=$page\">[Next]</a> ";
-            $last = " <a href=\"$self?page=$maxPage\">[Last Page]</a> ";
+            $next = " <a href='$self?page=$page'>Next</a> ";
+            $last = " <a href='$self?page=$maxPage'>Last</a> ";
         } else {
             $next = '&nbsp;';
             $last = '&nbsp;';
         }
-
-        echo "<center>$first "." $prev "." $nav "." $next "." $last</center>";
+    
+        echo "<center>$first"."$prev"."$nav"."$next"."$last</center>";
 
         mysqli_close($conn);
     ?>
