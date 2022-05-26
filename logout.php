6a52dc8f2e901e600e9cc29ab8b0ef2e9008e20d
Daniel Reynard <daniel.reynard.k@gmail.com>
2022-05-26 19:16:38 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'config.php';
-    session_start();
-    $_SESSION['sadmin_username'] = "";
-    $_SESSION['sadmin_nama'] = "";
-    header('location: index.php');
-    mysqli_close($conn);
-?>
